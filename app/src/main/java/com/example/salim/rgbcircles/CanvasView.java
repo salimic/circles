b6362dc54d58c4c4cbc839c7b661d7b651a2c005
salimic <salim@kadastr05.ru>
2019-01-18 18:18:18 +0300

    stage3

--- app/src/main/java/com/example/salim/rgbcircles/CanvasView.java
+++ app/src/main/java/com/example/salim/rgbcircles/CanvasView.java
@@ -3,16 +3,31 @@ package com.example.salim.rgbcircles;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class CanvasView extends View {
+    private static int width;
+    private static int height;
     private GameManager gameManager;
 
     public CanvasView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        gameManager=new GameManager();
+        initWidthAndHeight(context);
+        gameManager=new GameManager(this, width, height);
 
+    }
+
+    private void initWidthAndHeight(Context context) {
+        WindowManager windowManager=(WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display=windowManager.getDefaultDisplay();
+        Point point=new Point();
+        display.getSize(point);
+        width=point.x;
+        height=point.y;
     }
 
 
--- app/src/main/java/com/example/salim/rgbcircles/GameManager.java
+++ app/src/main/java/com/example/salim/rgbcircles/GameManager.java
@@ -9,9 +9,15 @@ import android.graphics.Paint;
 
 public class GameManager {
     private MainCircle mainCircle;
+    private CanvasView canvasView;
+    private static int width;
+    private static int height;
     private Paint paint;
 
-    public GameManager(){
+    public GameManager(CanvasView canvasView, int w, int h){
+        this.canvasView=canvasView;
+        width=w;
+        height=h;
         initMainCircle();
         initPaint();
     }
@@ -22,7 +28,7 @@ public class GameManager {
     }
 
     private void initMainCircle() {
-        mainCircle=new MainCircle(200, 500);
+        mainCircle=new MainCircle(width/2, height/2);
     }
 
     public void onDraw(Canvas canvas){
